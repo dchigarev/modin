f86f9270088152fb5252995985a374d69638de68
Dmitry Chigarev <dmitry.chigarev@intel.com>
2020-06-29 19:06:10 +0400

    fix
    Signed-off-by: Dmitry Chigarev <dmitry.chigarev@intel.com>

--- modin/backends/pandas/query_compiler.py
+++ modin/backends/pandas/query_compiler.py
@@ -1656,9 +1656,11 @@ class PandasQueryCompiler(BaseQueryCompiler):
 
         # This implementation can handle that case, but pandas raises exception.
         # Emulating pandas behaviour
-        if len(np.unique(keys + values)) < len(keys + values) and len(
-            keys + values
-        ) < len(self.columns):
+        if (
+            len(values)
+            and len(np.unique(keys + values)) < len(keys + values)
+            and len(keys + values) < len(self.columns)
+        ):
             raise ValueError("Keys overlapping")
 
         if len(values):
