91c4d64024232e86ecddb0186e0afa88a5d3d69a
Dmitry Chigarev <dmitry.chigarev@intel.com>
2020-07-09 12:22:31 +0300

    refactoring
    Signed-off-by: Dmitry Chigarev <dmitry.chigarev@intel.com>

--- modin/backends/pandas/query_compiler.py
+++ modin/backends/pandas/query_compiler.py
@@ -22,6 +22,7 @@ from pandas.core.base import DataError
 
 from modin.backends.base.query_compiler import BaseQueryCompiler
 from modin.error_message import ErrorMessage
+from modin.pandas.utils import _safe_index_creator
 from modin.data_management.functions import (
     FoldFunction,
     MapFunction,
@@ -139,118 +140,6 @@ def copy_df_for_func(func):
         return df
 
     return caller
-
-
-def _safe_index_creator(
-    values, like=None, level=0, positions=None, discard_bottom_levels=True
-):
-    """
-    Creates new index from `values` with the same parameters
-    as `like` (number of levels and its names).
-
-    Parameters
-    ----------
-    values : list of str, callable
-        list of labels of new index. If callable is passed, then
-        it function will be aplied to every label in `like` (considering
-        `level` and `positions`)
-
-    like : Index, optional default None
-
-    level : int, optional default 0
-        if `like` is MultiIndex `level` parameter could be passed to
-        specify the level to where labels from `values` should be inserted
-
-    positions : list of int, optional
-        positions where transformation with indices required (applying transform
-        function from `values` or inserting labels at specified level in `like`)
-        if not specifed applies transofrmation to all `values`
-
-    discard_bottom_levels : bool, optional default True
-        in case of MultiIndex `like` should we or not discard levels
-        which number is greater than `level` parameter
-
-    Returns
-    -------
-        New created Index
-
-    Examples
-    --------
-    >>> indices
-    MultiIndex([('a', 'one', 'foo', 'zoo'),
-            ('a', 'one', 'foo', 'lol'),
-            ('a', 'one', 'bar', 'zoo'),
-            ('a', 'one', 'bar', 'lol'),
-            ('a', 'two', 'foo', 'zoo')],
-           names=['first', 'second', 'third', 'fourth'])
-
-    >>> _safe_index_creator(['margin_name'], like=indices, positions=[0], level=1)
-    MultiIndex([('a', 'margin_name', '', '')],
-            names=['first', 'second', 'third', 'fourth'])
-
-    >>> _safe_index_creator(
-            ['margin_name'],
-            like=indices,
-            positions=[0],
-            level=1,
-            discard_bottom_levels=False
-        )
-    MultiIndex([('a', 'margin_name', 'foo', 'zoo')],
-            names=['first', 'second', 'third', 'fourth'])
-
-    >>> _safe_index_creator(lambda label: label[0], like=indices, level=2, positions=[0, 2, 4])
-    MultiIndex([('a', 'one',   'f',    ''),
-            ('a', 'one', 'foo', 'lol'),
-            ('a', 'one',   'b',    ''),
-            ('a', 'one', 'bar', 'lol'),
-            ('a', 'two',   'f',    '')],
-           names=['first', 'second', 'third', 'fourth'])
-
-    """
-    if like is None:
-        return pandas.Index(list(values))
-
-    if positions is None:
-        positions = np.arange(len(like))
-
-    is_miltiindex = isinstance(like, pandas.MultiIndex)
-
-    def dummy_func(x):
-        return x
-
-    if callable(values):
-        transform_func = values
-        values = list(like)
-        if is_miltiindex:
-            for i in positions:
-                values[i] = values[i][level]
-    elif is_miltiindex:
-        transform_func = dummy_func
-    else:
-        transform_func = None
-
-    if is_miltiindex:
-        for i in positions:
-            values[i] = (
-                (
-                    *like[i][:level],
-                    transform_func(values[i]),
-                    *([""] * (len(like[i]) - level - 1)),
-                )
-                if discard_bottom_levels
-                else (
-                    *like[i][:level],
-                    transform_func(values[i]),
-                    *like[i][(level + 1) :],
-                )
-            )
-    elif transform_func:
-        for i in positions:
-            values[i] = transform_func(values[i])
-
-    new_index = pandas.Index(list(values))
-    new_index.names = like.names
-    return new_index
 
 
 class PandasQueryCompiler(BaseQueryCompiler):
